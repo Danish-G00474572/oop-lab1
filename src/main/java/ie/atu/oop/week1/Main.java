96326c0de3fcd61df2ec970f396eb5138bc981dd
DanishFernandes-STUD <G00474572@atu.ie>
2026-09-21 12:28:49 +0100

    Created a new Maven project for week 1 with comment

--- src/main/java/ie/atu/oop/week1/Main.java
+++ src/main/java/ie/atu/oop/week1/Main.java
@@ -4,7 +4,7 @@ package ie.atu.oop.week1;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello OOP");
+        System.out.println("Hello OOP");//print
 
     }
 }

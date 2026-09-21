efbbc10c8127e2faa6d163fa6cd799db2ffa8449
DanishFernandes-STUD <G00474572@atu.ie>
2026-09-21 13:04:54 +0100

    Completed the library system

--- src/main/java/ie/atu/oop/week1/Book.java
+++ src/main/java/ie/atu/oop/week1/Book.java
@@ -5,4 +5,23 @@ public class Book {
     public String author;
     public int pageCount;
     public boolean available = true;
+
+    public static void desplayDetails() {
+
+        System.out.println(" book title: " + title);
+        System.out.println(" book author: " + author);
+        System.out.println(" book page count: " + pageCount);
+        System.out.println(" book avalable: " + available);
+        System.out.println("/n");
+
+    }
+    public void borrowBook() {
+        if (available) {
+            available = false;
+            System.out.println(title + " Avalable");
+        }
+        else{
+            System.out.println("Book Not Available");
+        }
+    }
 }
--- src/main/java/ie/atu/oop/week1/Main.java
+++ src/main/java/ie/atu/oop/week1/Main.java
@@ -6,5 +6,29 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello OOP");//print
 
+        Book firstbook =new Book();
+
+        firstbook.title = "Doom";
+        firstbook.author = "Daniel";
+        firstbook.pageCount = 304;
+        firstbook.available = true;
+
+        firstbook.desplayDetails();
+        firstbook.borrowBook();
+        firstbook.desplayDetails();
+
+
+        System.out.println("/n");
+
+        Book secondbook =new Book();
+        secondbook.title = "Star Trek";
+        secondbook.author = "Dave";
+        secondbook.pageCount = 135;
+        secondbook.available = true;
+
+        secondbook.desplayDetails();
+
+
+
     }
 }
